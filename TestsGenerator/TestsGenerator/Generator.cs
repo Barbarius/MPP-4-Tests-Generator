8c6ec77444bcf68bb011fec2c34eb98a43174dc7
Sergei <s.bysov@mail.ru>
2018-12-09 00:42:57 +0300

    Create async file writer

--- TestsGenerator/TestsGenerator/Generator.cs
+++ TestsGenerator/TestsGenerator/Generator.cs
@@ -7,7 +7,34 @@ using System.Threading.Tasks.Dataflow;
 
 namespace TestsGenerator
 {
-    class Generator
+    public class Generator
     {
+        private readonly int readerFilesCount;
+        private readonly int writerFilesCount;
+        private readonly FileReader fileReader;
+
+        public Task Generate(List<string> paths)
+        {
+            ExecutionDataflowBlockOptions readerOptions = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = readerFilesCount
+            };
+            ExecutionDataflowBlockOptions writerOptions = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = writerFilesCount
+            };
+
+            var readerTransformBlock = new TransformBlock<string, Task<string>>(readPath => fileReader.ReadFileAsync(readPath), readerOptions);
+
+            readerTransformBlock.Complete();
+        }
+
+        public Generator(int readerFilesCount, int writerFilesCount)
+        {
+            this.readerFilesCount = readerFilesCount;
+            this.writerFilesCount = writerFilesCount;
+
+            fileReader = new FileReader();
+        }
     }
 }
