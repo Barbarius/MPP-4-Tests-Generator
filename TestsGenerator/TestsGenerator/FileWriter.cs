52806e7a40de39378e71dc78d6e9cc9bf6090ae5
Sergei <s.bysov@mail.ru>
2018-12-11 18:18:54 +0300

    Add file writer class

--- TestsGenerator/TestsGenerator/FileWriter.cs
+++ TestsGenerator/TestsGenerator/FileWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace TestsGenerator
 {
     class FileWriter
     {
+        public async void WriteFileAsync(Task<List<GeneratedTest>> generatedTests)
+        {
+            var resultTests = await generatedTests;
+            foreach(var resultTest in resultTests)
+            {
+                using (StreamWriter sw = new StreamWriter(resultTest.Name))
+                {
+                    await sw.WriteAsync(resultTest.Text);
+                }
+            }
+        }
     }
 }
--- TestsGenerator/TestsGenerator/Generator.cs
+++ TestsGenerator/TestsGenerator/Generator.cs
@@ -34,9 +34,22 @@ namespace TestsGenerator
 
             //var writerTransformBlock = new TransformBlock<string, Task<string>>();
 
-            var generatorTransformBlock = new TransformBlock<Task<string>, >();
+            var generatorTransformBlock = new TransformBlock<Task<string>, Task<List<GeneratedTest>>>(
+                new Func<Task<string>, Task<List<GeneratedTest>>>(GenerateFileAsync), maxTasksOptions);
+
+
 
             readerTransformBlock.Complete();
+        }
+
+        private async Task<List<GeneratedTest>> GenerateFileAsync(Task<string> readSourseFile)
+        {
+            string sourceFile = await readSourseFile;
+            var result = new List<GeneratedTest>();
+
+            //
+
+            return result;
         }
 
         public Generator(int readerFilesCount, int writerFilesCount, int maxTasksCount)
